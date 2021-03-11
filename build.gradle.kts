829343deec5a28a528420a86e41cfd94396d0897
Alan Flores <alan@inventure.com>
2021-03-11 15:31:43 -0600

    Add unit test for key creation (#11)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ val ktorVersion = "1.3.2"
 val jacksonVersion = "2.11.3"
 val bouncyCastleVersion = "1.67"
 val kotlinReflectVersion = "1.4.30"
+val junitVersion = "5.3.1"
+val mockitoVersion = "3.4.6"
 
 group = groupId
 version = versionRelease
@@ -38,5 +40,39 @@ dependencies {
     implementation("org.bouncycastle:bcprov-jdk15on:$bouncyCastleVersion")
     implementation("org.bouncycastle:bcpkix-jdk15on:$bouncyCastleVersion")
 
-    testCompile("junit", "junit", "4.12")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+
+    testImplementation("org.mockito:mockito-core:$mockitoVersion")
+
+    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
+    testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
+    testImplementation("io.ktor:ktor-client-mock-js:$ktorVersion")
+    testImplementation("io.ktor:ktor-client-mock-native:$ktorVersion")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("skipped", "passed", "failed")
+            showStandardStreams = false
+            showExceptions = false
+            addTestListener(object : TestListener {
+                override fun beforeSuite(suite: TestDescriptor) {}
+                override fun beforeTest(testDescriptor: TestDescriptor) {}
+                override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {}
+                override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+                    if (suite.parent != null) {
+                        val output =
+                            "Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} passed, ${result.failedTestCount} failed, ${result.skippedTestCount} skipped)"
+                        val startItem = "|  "
+                        val endItem = "  |"
+                        val repeatLength = startItem.length + output.length + endItem.length
+                        println("\n${"-".repeat(repeatLength)}\n$startItem$output$endItem\n${"-".repeat(repeatLength)}")
+                    }
+                }
+            })
+        }
+    }
 }
